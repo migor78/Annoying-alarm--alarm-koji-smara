63d55f1114f0afdd3c31a92e29dd5f9040b1f116
Matija <bombmatija@gmail.com>
2023-07-09 20:23:21 +0200

    Initial commit. Kreiran projekat

---
+++ app/src/main/java/com/example/annoyingalarm/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.annoyingalarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("KORISNIK RADI SKLEKOVE")
+    }
+}

a1787a60fe6775d51b308a728e9e635c5a6ab68d
Matija <bombmatija@gmail.com>
2023-07-17 21:11:28 +0200

    Testiranje Toast notifikacija

--- app/src/main/java/com/example/annoyingalarm/MainActivity.kt
+++ app/src/main/java/com/example/annoyingalarm/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.annoyingalarm
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.annoyingalarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val hiText = findViewById<TextView>(R.id.HiText)
+        val ime = findViewById<EditText>(R.id.Ime)
+        val logDugme = findViewById<Button>(R.id.LogButton)
+        logDugme.setOnClickListener {
+            if(ime.text.toString()!="")
+                hiText.text = "Hello ${ime.text}!"
+            else {Toast.makeText(this@MainActivity, "Niste uneli ime!", Toast.LENGTH_SHORT).show(); hiText.text = "HELLO ANDROID!"}
+            ime.text.clear()
+        }
     }
 }
